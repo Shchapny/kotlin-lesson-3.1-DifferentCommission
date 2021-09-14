73e414462fdd27d15f763e7797c30ec7385ff22a
Rifmir Gatin <rifmir349@yandex.ru>
2021-09-14 22:01:18 +0300

    Рефакторинг кода

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -16,24 +16,30 @@ fun main() {
 
 fun commissionMastercardAndMaestro(amountDay: Long, amountMonth: Long): String {
     val sum = ((amountDay / 100 * 0.6) + 2_000)
-    return if (amountDay in 30_000..7_500_00) "Без комиссии"
-    else if (amountDay >= MAX_AMOUNT_DAY) "Сумма превышает дневной лимит!"
-    else if (amountMonth >= MAX_AMOUNT_MONTH) "Превышен лимит за месяц!"
-    else "Комиссия $sum коп."
+    return when {
+        amountDay in 30_000..7_500_00 -> "Без комиссии"
+        amountDay >= MAX_AMOUNT_DAY -> "Сумма превышает дневной лимит!"
+        amountMonth >= MAX_AMOUNT_MONTH -> "Превышен лимит за месяц!"
+        else -> "Комиссия $sum коп."
+    }
 }
 
 fun commissionVisaAndMir(amountDay: Long, amountMonth: Long): String {
     val sum = (amountDay / 100 * 0.75)
-    return if (amountDay <= MIN_AMOUNT_VISA_MIR) "Комиссия 35 руб."
-    else if (amountDay >= MAX_AMOUNT_DAY) "Сумма превышает дневной лимит!"
-    else if (amountMonth >= MAX_AMOUNT_MONTH) "Превышен лимит за месяц!"
-    else "Комиссия $sum коп."
+    return when {
+        amountDay <= MIN_AMOUNT_VISA_MIR -> "Комиссия 35 руб."
+        amountDay >= MAX_AMOUNT_DAY -> "Сумма превышает дневной лимит!"
+        amountMonth >= MAX_AMOUNT_MONTH -> "Превышен лимит за месяц!"
+        else -> "Комиссия $sum коп."
+    }
 }
 
 fun commissionVkPay(amountDay: Long, amountMonth: Long): String {
-    return if (amountDay >= MAX_AMOUNT_DAY_VK_PAY) "Сумма превышает дневной лимит!"
-    else if (amountMonth >= MAX_AMOUNT_MONTH_VK_PAY) "Превышен лимит за месяц"
-    else "Без комиссии"
+    return when {
+        amountDay >= MAX_AMOUNT_DAY_VK_PAY -> "Сумма превышает дневной лимит!"
+        amountMonth >= MAX_AMOUNT_MONTH_VK_PAY -> "Превышен лимит за месяц"
+        else -> "Без комиссии"
+    }
 }
 
 fun commission(bankCard: String = VK_PAY, amountDay: Long, amountMonth: Long = 0): String {
